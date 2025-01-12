5f763f8a81fa9c5fd0d2e4b93e7a041d83f25692
vlhong97 <vlhong@uci.edu>
2025-01-12 15:59:05 -0800

    changed plantseed()

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -8,6 +8,10 @@ public class PlantCountUI : MonoBehaviour
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        //_text.text = "Points: " + points.ToString();
+
+        _plantedText.text = seedsPlanted.ToString();
+
+        _remainingText.text = seedsLeft.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,11 +18,36 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        // Player moves up, down, left, right depending on W, S, A, D keyboard input
+        if (Input.GetKey(KeyCode.W))
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            _playerTransform.Translate(Vector3.left * _speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+
+        // Player plants seed when pressing space
+        if (Input.GetKey(KeyCode.Space))
+            PlantSeed(); 
     }
 
     public void PlantSeed ()
     {
-        
+        // Spawns plant 
+        Instantiate(_plantPrefab);
+
+        _numSeeds -= 1; 
+
+        _numSeedsLeft = _numSeeds;
+
+        _numSeedsPlanted += 1; 
+
+
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
