3cf9651f8c71a08a837e6da48a090f8973e3c99d
vlhong97 <vlhong@uci.edu>
2025-01-13 22:01:07 -0800

    seeds planted at player's location

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -8,10 +8,14 @@ public class PlantCountUI : MonoBehaviour
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        //_text.text = "Points: " + points.ToString();
+        // check on console if the PlantCountUI script got the data of the amount of seeds left/planted
+        Debug.Log("PlantCountUI has " + seedsLeft);
+        Debug.Log("PlantCountUI has " + seedsPlanted);
 
+        // update UI for amount of seeds planted.
         _plantedText.text = seedsPlanted.ToString();
-
+        
+        // update UI for amount of seeds left in inventory.
         _remainingText.text = seedsLeft.ToString();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -11,14 +12,25 @@ public class Player : MonoBehaviour
     private int _numSeedsLeft;
     private int _numSeedsPlanted;
 
-    private void Start ()
+    private void Start()
     {
-        
+        // all this must be done first in Start() before the game begins.
+        // establishes the number of seeds the player has & the lack of seeds planted.
+        _numSeedsLeft = _numSeeds;
+        _numSeedsPlanted = 0;
+
+        // check on console if the amount has been set correctly.
+        Debug.Log("Number of seeds left : " + _numSeedsLeft);
+        Debug.Log("Number of seeds planted : " + _numSeedsPlanted);
+
+        // updates the UI to show number of seeds left/planted.
+        _plantCountUI = gameObject.AddComponent<PlantCountUI>();
+        _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 
     private void Update()
     {
-        // Player moves up, down, left, right depending on W, S, A, D keyboard input
+        // Player moves up, down, left, right depending on W, S, A, D keyboard input.
         if (Input.GetKey(KeyCode.W))
             _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
 
@@ -31,23 +43,25 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
             _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
 
-        // Player plants seed when pressing space
-        if (Input.GetKey(KeyCode.Space))
+
+        // Player plants seed when pressing space.
+        if (_numSeedsLeft > 0 && Input.GetKeyDown(KeyCode.Space))
             PlantSeed(); 
     }
 
     public void PlantSeed ()
     {
-        // Spawns plant 
-        Instantiate(_plantPrefab);
-
-        _numSeeds -= 1; 
-
+        // spawns prefab directly where the player's position is
+        Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+        
+        // subtracts one seed from the number of seeds the player has left
+        _numSeeds = _numSeeds - 1; 
         _numSeedsLeft = _numSeeds;
 
-        _numSeedsPlanted += 1; 
+        // adds the amount of seeds planted on the main scene
+        _numSeedsPlanted = _numSeedsPlanted + 1;
 
-
+        // updates the UI text 
         _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
     }
 }
